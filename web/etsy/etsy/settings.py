b0f29f393c184a2ae4abb5489db95a9978b8f71c
Eudald Arranz <eudald.arranz@nested.net>
2018-09-29 19:04:13 +0200

    #27 Minor nginx configuration change - collectstatic #4 Finishing the user model and forms

--- web/etsy/etsy/settings.py
+++ web/etsy/etsy/settings.py
@@ -145,5 +145,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+PROJECT_PATH = os.path.abspath(os.path.dirname(__name__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
