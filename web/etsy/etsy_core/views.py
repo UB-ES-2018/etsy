2f1415b52d7db575d1494f36d2601f2ef0bec0ca
Eudald Arranz <eudald.arranz@nested.net>
2018-10-03 17:43:53 +0200

    [US-1] Created the signup logic @thefogui Added the template schema with the django form

--- web/etsy/etsy_core/views.py
+++ web/etsy/etsy_core/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 from django.http import HttpResponse
-
+from .forms import RegisterForm
 # Create your views here.
 
 
 def index(request):
     return render(request, 'home.html', {})
+
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data.get('email')
+            raw_password = form.cleaned_data.get('password2')
+            user = authenticate(email=email, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = RegisterForm()
+    return render(request, 'signup.html', {'form': form})
