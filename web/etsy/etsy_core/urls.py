b6965604afc6df27c7cfc59a8bf8010e452b65bb
Oscar Lima <oscar_bozz@hotmail.com>
2018-10-04 22:00:51 +0200

    Defined urls for product creation/visualization

--- web/etsy/etsy_core/urls.py
+++ web/etsy/etsy_core/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path(r'^shop/$', views.create_shop, name='shop'),
     path(r'^shop/(?P<shop_id>.*)/', views.shop, name='shop'),
+    path(r'^shop/(?P<shop_id>.*)/product/$', views.create_product, name='shop'),
+    path(r'^shop/(?P<shop_id>.*)/product/(?P<product_id>.*)/', views.product, name='shop')
 ]
