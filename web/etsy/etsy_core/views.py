fec891a26b0cef60519340b7ad66b0f29f8f11ce
Oscar Lima <oscar_bozz@hotmail.com>
2018-10-05 22:02:53 +0200

    Changed product view. Now it uses a single method to list all products of a shop and to create a new one for that shop checking request method

--- web/etsy/etsy_core/views.py
+++ web/etsy/etsy_core/views.py
@@ -13,8 +13,9 @@ def shop(request, shop_id):
 def create_shop(request):
     return render(request, '', {})
 
-def product(request, product_id):
-    return render(request, '', {})
-
-def create_product(request):
+def products(request, shop_id):
+    if request.method == 'GET':
+        #Get all products of a shop
+    else if request.method == 'POST':
+        #Create a new product of that shop
     return render(request, '', {})
