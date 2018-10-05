37c9c5b4ba6aef5c35e32d90429f50b7be1e6142
Eudald Arranz <eudald.arranz@nested.net>
2018-10-05 10:55:49 +0200

    [US-3] #15 first approach

--- web/etsy/etsy_core/views.py
+++ web/etsy/etsy_core/views.py
@@ -6,3 +6,17 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'home.html', {})
+
+
+def login(request):
+    if request.method == 'POST':
+        previous_page = request.REQUEST.get('next', '')
+        username = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(previous_page)
+        else:
+            
+    else:
