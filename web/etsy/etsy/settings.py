03ecda950a54eb8c22cb60652da3df090ab7d045
Eudald Arranz <eudald.arranz@nested.net>
2018-10-09 11:27:59 +0200

    [CR] Fixing the linking between frontend and backend - login form working

--- web/etsy/etsy/settings.py
+++ web/etsy/etsy/settings.py
@@ -64,7 +64,11 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries':{
+                'myfilters': 'etsy_core.templatetags.myfilters',
+            }
         },
+
     },
 ]
 
