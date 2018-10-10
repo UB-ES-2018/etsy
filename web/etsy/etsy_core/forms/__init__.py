a33eef1ef8d0cf72895c348af5cf55ed73c8dee5
Eudald Arranz <eudald.arranz@nested.net>
2018-10-10 13:27:25 +0200

    Integration errors

--- web/etsy/etsy_core/forms/__init__.py
+++ web/etsy/etsy_core/forms/__init__.py
@@ -1,6 +1,3 @@
 from .UserForms import UserAdminChangeForm, UserAdminCreationForm, RegisterForm, LoginForm
 from .shopForms import ShopForm
-<<<<<<< HEAD
-=======
 from .productForms import ProductForm
->>>>>>> US-4.1-BE-Logic_product_creation
--- web/etsy/etsy_core/models/__init__.py
+++ web/etsy/etsy_core/models/__init__.py
@@ -1,6 +1,3 @@
 from .user import User
 from .shop import Shop
-<<<<<<< HEAD
-=======
 from .product import Product
->>>>>>> US-4.1-BE-Logic_product_creation
