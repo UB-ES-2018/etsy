29a73a041dc65b13fa5796d35e981549a3bcd226
vitor <vitorthefogui@gmail.com>
2018-10-23 18:15:21 +0200

    Need some css and integrate with the backend

--- web/etsy/static/admin/js/shop.js
+++ web/etsy/static/admin/js/shop.js
@@ -3,43 +3,29 @@ var left, opacity, scale; //fieldset properties wich will be animate
 var animating; 
 
 $(".next").click(function(){
-    if (animating) 
-        return false;
     
-    animating = true;
     currentFs = $(this).parent();
-    alert($(this).parent());
-    nextFs = $(this).parent().next();
+    
+    nextFs = currentFs.next();
 
     $("#progressbar li").eq($("fieldset").index(nextFs)).addClass("active");
 
     nextFs.show();
-
-    currentFs.animate({
-        opacity:0
-        }, 
-        {step: function(now, mx) {
-            sacle = 1 - (1 - now) *  0.2;
-            left = (now * 50) + "%";
-            opacity = 1 - now;
-
-            currentFs.css({
-                'transform': 'scale('+scale+')',
-                'position': 'absolute'
-            });
-
-            nextFs.css({
-                'left': left,
-                'opacity': opacity
-            });
-        },
-
-        duration: 800,
-        complete: function(){
-            currentFs.hide();
-            animating = false;
-        },
-        easing: 'easeInOutBack'
-    });
+    currentFs.hide();
 });
 
+$(".previous").click(function(){
+	if(animating) return false;
+	animating = true;
+	
+	current_fs = $(this).parent();
+	previous_fs = $(this).parent().prev();
+	
+	//de-activate current step on progressbar
+	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+	
+	//show the previous fieldset
+	previous_fs.show(); 
+	//hide the current fieldset with style
+	
+});
