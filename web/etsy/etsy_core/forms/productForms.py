d2655955ba754efe6adf7937e910da24dc0861f5
Oscar Lima <oscar_bozz@hotmail.com>
2018-10-23 02:01:52 +0200

    Adding tag logic in product form

--- web/etsy/etsy_core/forms/productForms.py
+++ web/etsy/etsy_core/forms/productForms.py
@@ -1,11 +1,12 @@
 from django import forms
-from ..models import Product, Shop, Options
+from ..models import Product, Shop, Options, Tags
 from ..services import VariationsHandler
 
 
 class ProductForm(forms.ModelForm):
     name = forms.CharField(required=True)
     description = forms.CharField(required=True)
+    tags = forms.CharField(required=True)
 
     class Meta:
         model = Product
@@ -29,6 +30,10 @@ class ProductForm(forms.ModelForm):
             field_name = f"option_{option.id}"
             self.fields[field_name] = forms.BooleanField(required=False, label=f"{option.options_name}")
 
+        self.fields['tags'].widget.attrs.update({
+               'data-role': "tagsinput",
+           })
+        
     def save(self, commit=True):
         product = super(ProductForm, self).save(commit=False)
         product.shop_id = self._shop
@@ -36,6 +41,7 @@ class ProductForm(forms.ModelForm):
         if commit:
             product.save()
             self.update_options(product)
+            self.update_tags(product)
             self.save_m2m()
 
         return product
@@ -52,3 +58,13 @@ class ProductForm(forms.ModelForm):
                 variation = Options.objects.get(id=option_id)
                 product = Product.objects.get(id=product.id)
                 VariationsHandler.add_variations_to_product(product, variation)
+
+    def update_tags(self, product):
+        if self.cleaned_data.get(tag_name):
+            product = Product.objects.get(id=product.id)
+            for tag_name in self.tags.split(','):
+                try:
+                    tag = Tags.objects.get(tags_name=tag_name)
+                except:
+                    tag = Tags(tags_name=tag_name)
+                VariationsHandler.add_tags_to_product(product, tag)
