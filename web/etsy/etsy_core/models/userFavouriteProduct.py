f1369c0e33202b6915e00dce34b3fd39dcdc6670
Oscar Lima <oscar_bozz@hotmail.com>
2018-11-20 01:18:28 +0100

    Fixing minor errors. Making migrations and migrating

--- web/etsy/etsy_core/models/userFavouriteProduct.py
+++ web/etsy/etsy_core/models/userFavouriteProduct.py
@@ -2,6 +2,6 @@ from django.db import models
 from .user import User
 from .product import Product
 
-class UserFavouriteShop(models.Model):
+class UserFavouriteProduct(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
