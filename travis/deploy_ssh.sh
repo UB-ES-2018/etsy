4817d8b0bbf50445c684e2cc0f7fc878843a7f3b
Eudald Arranz <eudald.arranz@nested.net>
2018-11-23 13:32:06 +0100

    new test

--- travis/deploy_ssh.sh
+++ travis/deploy_ssh.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export SSHPASS=$DEPLOY_PASS
-sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/web/minideploy.sh
+sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST "sh $DEPLOY_PATH/web/minideploy.sh"
