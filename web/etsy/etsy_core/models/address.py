a139ebef48468fde480cfff34b2e59b931ac77e7
Borja <bhidalto12@alumnes.ub.edu>
2018-12-09 16:20:42 +0100

    id added to model

--- web/etsy/etsy_core/models/address.py
+++ web/etsy/etsy_core/models/address.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.validators import MaxValueValidator
 
 class Address(models.Model):
+    id = models.AutoField(primary_key=True)
     zipcode = models.IntegerField(verbose_name='zipcode',validators=[MaxValueValidator(99999)])
     city = models.CharField(verbose_name='city',max_length=50)
     country = models.CharField(verbose_name='country',max_length=50)
