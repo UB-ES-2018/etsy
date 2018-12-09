2936bb28e6d6d55a819f59a6652f0b46cb25cbed
Borja <bhidalto12@alumnes.ub.edu>
2018-12-09 12:42:08 +0100

    Address model

---
+++ web/etsy/etsy_core/models/address.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.core.validators import MaxValueValidator
+
+class Address(models.Model):
+    zipcode = models.IntegerField(verbose_name='zipcode',required=True,validators=[MaxValueValidator(99999)])
+    city = models.CharField(verbose_name='city',max_length=50,required=True)
+    country = models.CharField(verbose_name='country',max_length=50, required=True)
+    street = models.charField(verbose_name='street_name',max_length=50, required=True)
+
+
+
+    def __str__(self):
+        return f"{self.street}, {self.zipcode} {self.city} - {self.country}"
