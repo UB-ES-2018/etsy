711d79413943a9362b9e75128fa7935fca36f08b
Borja <bhidalto12@alumnes.ub.edu>
2018-12-11 23:44:09 +0100

    Check for address added

--- web/etsy/etsy_core/models/address.py
+++ web/etsy/etsy_core/models/address.py
@@ -3,12 +3,15 @@ from django.core.validators import MaxValueValidator
 
 class Address(models.Model):
     id = models.AutoField(primary_key=True)
-    zipcode = models.IntegerField(verbose_name='zipcode',validators=[MaxValueValidator(99999)])
-    city = models.CharField(verbose_name='city',max_length=50)
-    country = models.CharField(verbose_name='country',max_length=50)
-    street = models.CharField(verbose_name='street_name',max_length=50)
+    zipcode = models.IntegerField(verbose_name='zipcode',validators=[MaxValueValidator(99999)],null=True,default=False)
+    city = models.CharField(verbose_name='city',max_length=50,null=True,default=False)
+    country = models.CharField(verbose_name='country',max_length=50,null=True,default=False)
+    street = models.CharField(verbose_name='street_name',max_length=50,null=True,default=False)
 
 
 
     def __str__(self):
         return f"{self.street}, {self.zipcode} {self.city} - {self.country}"
+
+    def is_valid(self):
+        return self.zipcode != None and self.city != None and self.country != None and self.street != None
