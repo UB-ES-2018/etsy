a851520afa54fc891518a4629d70ffccab1286ab
root <oscar_bozz@hotmail.com>
2018-12-11 16:37:23 +0100

    Fixing bugs. Changing view

--- web/etsy/etsy_core/forms/shopForms.py
+++ web/etsy/etsy_core/forms/shopForms.py
@@ -45,7 +45,7 @@ class LogoUploadForm(forms.Form):
     """Image upload form."""
     image = forms.ImageField()
 
-class ShopUpdateForm(forms.ModelForm):
+class ShopUpdateForm(forms.Form):
     name = forms.CharField()
     country = forms.ChoiceField(choices=[(1, 'ES'), (2, 'UK')])
     currency = forms.ChoiceField(choices=[(1, 'EUR'), (2, 'GBP')])
@@ -56,23 +56,5 @@ class ShopUpdateForm(forms.ModelForm):
         if qs.exists():
             raise forms.ValidationError("Shop name already taken")
         return name
-
-    def __init__(self, *args, **kwargs):
-        self._user = kwargs.pop('user', None)
-        if (self._user is not None):
-            User.objects.get(email=self._user.email)
-        super(ShopForm, self).__init__(*args, **kwargs)
-
-    def save(self, commit=True):
-        inst = super(ShopForm, self).save(commit=False)
-        inst.shop_owner = self._user
-        inst.name = self.cleaned_data.get('name')
-        if (not self._user.has_shop):
-            self._user.has_shop = True
-            self._user.save()
-        if commit:
-            inst.save()
-            self.save_m2m()
-        return inst
 
 
--- web/etsy/etsy_core/views/views.py
+++ web/etsy/etsy_core/views/views.py
@@ -370,6 +370,11 @@ def update_shop(request, shop_id):
 	if request.method == 'POST':
 		form = ShopUpdateForm(request.POST, request.FILES)
 		if form.is_valid():
-			form.save(commit=True)
+			shop = Shop.objects.get(id=shop_id)
+			shop.name = form.cleaned_data['name']
+			shop.language = form.cleaned_data['language']
+			shop.country = form.cleaned_data['country']
+			shop.currency = form.cleaned_data['currency']
+			shop.save()
 			return redirect('/shop/' + (str)(shop_id))
 	return render(request, 'shop_edit.html', {'form': form})
